470f36da9e71f9fc571ae0d596cdaa8634ca204f
Ahmet Gencoglu <agencogl@uncc.edu>
2016-09-14 15:09:14 -0400

    Remove unused imports

--- app/src/main/java/com/group32/hw2/DeleteExpenseActivity.java
+++ app/src/main/java/com/group32/hw2/DeleteExpenseActivity.java
@@ -7,13 +7,10 @@ import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ImageView;
-import android.widget.ListAdapter;
 import android.widget.Spinner;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import java.text.DateFormat;
